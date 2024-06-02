128174d46031b1ad251bbef1e63c2d529979e6ed
Harsh Kumar <hk10539@gmail.com>
2024-06-02 16:33:30 +0530

    Commit2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,14 @@
 package org.example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("HK");
-        System.out.println("HK");
+        ChromeOptions co=new ChromeOptions();
+        co.addArguments("--remote-allow-origins=*");
+        WebDriver driver=new ChromeDriver(co);
+        driver.get("https://www.google.com");
     }
 }
